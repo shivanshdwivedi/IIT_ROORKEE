98b2a7b48af83205e6b211f762a9d1ad6e3494dd
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-02-02 01:24:45 +0530

    commit

--- app.js
+++ app.js
@@ -19,7 +19,17 @@ app.get("/", function(req, res){
     res.render("page 1");
 });
 
-app.get("/")
+app.get("/page2" , function(req,res){
+    res.render("page2");
+});
+
+app.get("/page3" , function(req,res){
+    res.render("page3");
+});
+
+app.get("/page4" , function(req,res){
+    res.render("page4");
+});
 
 var server = app.listen(process.env.PORT || 3000, function () {
     var port = server.address().port;
