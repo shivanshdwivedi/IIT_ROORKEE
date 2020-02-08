fbe0d940d571d617d889b1e85f7201c56507524d
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-02-08 14:31:39 +0530

    commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-// mongoose.connect("mongodb://localhost:27017/officedb" ,{useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost:27017/officedb" ,{useNewUrlParser: true , useUnifiedTopology: true});
 
 var transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -28,14 +28,17 @@ var transporter = nodemailer.createTransport({
 console.log('created');
 
 
-// // const officeSchema = new mongoose.Schema({
+const officeSchema = new mongoose.Schema({
 
-// //     name : String,
-// //     password : String
+    name : String,
+    phone : Number,
+    department : String,
+    email :String ,
+    password : String
 
-// //    });
+   });
   
-// //   const employee = mongoose.model("employee" , officeSchema);
+const employee = mongoose.model("employee" , officeSchema);
 
 app.get("/", function(req, res){
     res.render("page 1");
@@ -58,21 +61,29 @@ app.get("/page5" , function(req,res){
 });
 
 
-// app.post("/page2" , function(req ,res){
-    //const Employee = new employee({
-        //name = req.body.name,
-        // phone : req.body.phone,
-        // department : req.body.department,
-        // email : req.body.email,
-        // password : req.body.password
-    //});
-    // Employee.save(function(err){
-    //     if(!err){
-    //         res.render("page3");
-    //     }
-    // }); 
+ app.post("/page2" , function(req ,res){
+    const Employee = new employee({
+        name : req.body.name,
+        phone : req.body.phone,
+        department : req.body.department,
+        email : req.body.email,
+        password : req.body.password
+    });
+    Employee.save(function(err){
+        if(!err){
+            res.render("page3");
+        }
+    }); 
 
-// });
+   console.log(req.body.name);
+   console.log(req.body.phone);
+   console.log(req.body.department);
+   console.log(req.body.email);
+   console.log(req.body.password);
+
+ });
+
+
 
  app.post('/page4' , (req,res) => {
 
@@ -93,21 +104,21 @@ app.get("/page5" , function(req,res){
 //         text: 'Hiiii',
 
 
-         var mailOptions = {
-           from: 'shivdwi043@gmail.com',
-           to: 'shrdwi043@gmail.com',
-           subject: 'pickup order for waste',
-           text: req.body.a1,
-  };
+//          var mailOptions = {
+//            from: 'shivdwi043@gmail.com',
+//            to: 'shrdwi043@gmail.com',
+//            subject: 'pickup order for waste',
+//            text: req.body.a1,
+//   };
   
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
-         res.redirect('/page5');
+//   transporter.sendMail(mailOptions, function(error, info){
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//     }
+//   });
+//          res.redirect('/page5');
      
         
       
@@ -120,8 +131,10 @@ app.get("/page5" , function(req,res){
     //      }
     //    });
 
-
+    console.log(req.body.a1);
  });
+
+
 
 
 
